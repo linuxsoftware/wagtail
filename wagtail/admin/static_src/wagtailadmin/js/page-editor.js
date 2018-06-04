09a1f950d625b4a214675856503ea01e775cd3ee
David <david@linuxsoftware.co.nz>
2018-06-05 10:43:01 +1200

    Choose one of the default datetimepicker locales

--- wagtail/admin/static_src/wagtailadmin/js/page-editor.js
+++ wagtail/admin/static_src/wagtailadmin/js/page-editor.js
@@ -32,10 +32,6 @@ function initDateChooser(id, opts) {
             timepicker: false,
             scrollInput: false,
             format: 'Y-m-d',
-            i18n: {
-                lang: window.dateTimePickerTranslations
-            },
-            lang: 'lang',
             onGenerate: hideCurrent
         }, opts || {}));
     } else {
@@ -54,11 +50,7 @@ function initTimeChooser(id) {
             closeOnDateSelect: true,
             datepicker: false,
             scrollInput: false,
-            format: 'H:i',
-            i18n: {
-                lang: window.dateTimePickerTranslations
-            },
-            lang: 'lang'
+            format: 'H:i'
         });
     } else {
         $('#' + id).datetimepicker({
@@ -74,10 +66,6 @@ function initDateTimeChooser(id, opts) {
             closeOnDateSelect: true,
             format: 'Y-m-d H:i',
             scrollInput: false,
-            i18n: {
-                lang: window.dateTimePickerTranslations
-            },
-            lang: 'lang',
             onGenerate: hideCurrent
         }, opts || {}));
     } else {
